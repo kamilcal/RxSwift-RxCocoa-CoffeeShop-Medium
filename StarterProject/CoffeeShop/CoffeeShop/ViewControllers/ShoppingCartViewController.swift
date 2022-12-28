577edc8cc29b2bd4312638b39d68fcf691b70aee
kamilcal <kamilcaal@gmail.com>
2022-12-28 19:51:59 +0300

    RxSwift and RxCocoa implemented on the ShoppingCartViewController

--- StarterProject/CoffeeShop/CoffeeShop/ViewControllers/ShoppingCartViewController.swift
+++ StarterProject/CoffeeShop/CoffeeShop/ViewControllers/ShoppingCartViewController.swift
@@ -7,83 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ShoppingCartViewController: BaseViewController {
-  
-  @IBOutlet private weak var tableView: UITableView!
-  @IBOutlet private weak var totalPriceLabel: UILabel!
-  
-  var cartItems: [CartItem] = []
-  var totalPrice: Float = 0 {
-    didSet {
-      if viewIfLoaded != nil {
-        totalPriceLabel.text = CurrencyFormatter.turkishLirasFormatter.string(from: totalPrice)
-      }
-    }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    configureTableView()
-    loadData()
-  }
-  
-  private func loadData() {
-    cartItems = ShoppingCart.shared.getCartItems()
-    totalPrice = ShoppingCart.shared.getTotalCost()
-  }
-  
-  private func removeCartItem(at row: Int) {
-    guard row < cartItems.count else { return }
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var totalPriceLabel: UILabel!
     
-    ShoppingCart.shared.removeCoffee(cartItems[row].coffee)
+    private let disposeBag = DisposeBag()
     
-    loadData()
-  }
-  
-  private func configureTableView() {
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: CGFloat.leastNonzeroMagnitude))
-    tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: CGFloat.leastNonzeroMagnitude))
-  }
-}
-
-extension ShoppingCartViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 104
-  }
-  
-  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    return true
-  }
-  
-  func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
-    return .delete
-  }
-  
-  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-      removeCartItem(at: indexPath.row)
-      tableView.deleteRows(at: [indexPath], with: .fade)
-    }
-  }
-}
-
-extension ShoppingCartViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return cartItems.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if let cell = tableView.dequeueReusableCell(withIdentifier: "cartCoffeeCell", for: indexPath) as? CartCoffeeCell {
-      cell.configure(with: cartItems[indexPath.row])
-      
-      return cell
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+        
+        // Rx - Setup
+        ShoppingCart.shared.getCartItems()
+            .bind(to: tableView
+                .rx
+                .items(cellIdentifier: "cartCoffeeCell", cellType: CartCoffeeCell.self)) { row, element, cell in
+                    cell.configure(with: element)
+                }
+                .disposed(by: disposeBag)
+        
+        ShoppingCart.shared.getTotalCost()
+            .subscribe(onNext: { [weak self] totalCost in
+                self?.totalPriceLabel.text = CurrencyFormatter.turkishLirasFormatter.string(from: totalCost)
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .modelDeleted(CartItem.self)
+            .subscribe(onNext: { cartItem in
+                        //Bu Observable değişkeni subscribe(onNext:) metodun içindeki closure’a paslarız. Bu closure her cell silindiğinde çalışacaktır.
+                ShoppingCart.shared.removeCoffee(cartItem.coffee) // 3
+            })
+            .disposed(by: disposeBag)
     }
     
-    return UITableViewCell()
-  }
+    private func configureTableView() {
+        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: CGFloat.leastNonzeroMagnitude))
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: CGFloat.leastNonzeroMagnitude))
+        tableView.rowHeight = 104
+    }
 }
-
